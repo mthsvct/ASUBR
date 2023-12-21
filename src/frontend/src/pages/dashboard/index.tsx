98e555943688038bb93d784796bd7987f00f0eae
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-12-21 19:22:38 -0300

    FEAT: Listagem de disciplinas na interface.

--- src/frontend/src/pages/dashboard/index.tsx
+++ src/frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/button"
+import { Footer } from "@/components/footer"
 import { canSSRAuth } from "@/utils/canSSRAuth"
 import Head from "next/head"
 
@@ -6,7 +8,9 @@ export default function Dashboard() {
 		<>
 			<Head><title>Home</title></Head>
 			<h1>Hello World!</h1>
+			<Button linkagem="/disciplinas">Disciplinas</Button>
 			<p><i>Dashboard</i></p>
+			<Footer />
 		</>
 	)
 }
--- src/frontend/src/pages/disciplinas/index.tsx
+++ src/frontend/src/pages/disciplinas/index.tsx
@@ -1,16 +1,70 @@
 import { useState, useContext, useEffect } from "react";
-// import { api }
 import Head from "next/head";
 import { Footer } from "@/components/footer";
+import { api } from "@/services/apiClient";
+import { toast } from "react-toastify";
 
+
+function Disciplina({disciplina, index}){
+    return (
+        <div key={index}>
+            <h1>{disciplina.name}</h1>
+            <p>{disciplina.codigo}</p>
+            <p>{disciplina.horas}h</p>
+        </div>
+    )
+}
+
+function Listagem({disciplinas}){
+    return disciplinas.map(
+        (disciplina, index) => {
+            return Disciplina({disciplina, index});
+        }
+    )
+}
 
 
 export default function Disciplinas() {
-    return (
-        <>  
-            <Head><title>Disciplinas</title></Head>
-            <h1>Hello World!</h1>
-            <Footer />
-        </>
-    )
+
+    const [disciplinas, setDisciplinas] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        api.get('/disciplinas/resumidas').then(
+            response => {
+                setDisciplinas(response.data);
+                setLoading(false);
+            }
+        ).catch(
+            error => {
+                console.log(error);
+                toast.error("Erro ao carregar disciplinas");
+            }
+        )
+    })
+
+
+    if (loading) {
+        return (
+            <>  
+                <Head><title>Disciplinas</title></Head>
+                <h1>Carregando...</h1>
+                <Footer />
+            </>
+        )
+    } else {
+        return (
+            <>  
+                <Head><title>Disciplinas</title></Head>
+                <div>
+                    <Listagem disciplinas={disciplinas} />
+                </div>
+                <Footer />
+            </>
+        )
+    }
+
+
+    
 }
